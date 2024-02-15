1a0a8b83030731dff0d2b7647fff8f7e762b7977
Zack Hoff <zackhoff42@gmail.com>
2024-02-14 19:16:57 -0600

    completed app files

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,40 @@
-# Put your app in here.
+from operations import add, sub, mult, div
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_route():
+    """Add a and b params."""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return str(add(a,b))
+
+@app.route('/sub')
+def sub_route():
+    """Subtract a and b params."""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return str(sub(a,b))
+
+@app.route('/mult')
+def mult_route():
+    """Multiply a and b params"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return str(mult(a,b))
+
+@app.route('/div')
+def div_route():
+    """Divide a and b params"""
+    
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return str(div(a,b))
